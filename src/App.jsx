abfdb5ecf290719662e82036fba0b303c5cc2865
AqulaR <103633654+AqulaR@users.noreply.github.com>
2024-01-19 22:50:41 +0300

    some changes for DO

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import Task from './task'
-import axios from 'axios';
+import Task from "./task";
+import axios from "axios";
 
-import './App.css'
+import "./App.css";
 
 function App() {
   const [tasks, setTasks] = useState([]);
   // const [tasksToServer, setTasksToServer] = useState([]);
-  const [tasksFromServer, setTasksFromServer] = useState("");
+  // const [tasksFromServer, setTasksFromServer] = useState("");
 
   // useEffect(() => {
   //   localStorage.setItem('todos', JSON.stringify(state))
   // }, [state]);
 
   useEffect(() => {
-    axios.get("http://localhost:3000/api/todos").then((data) => {
-      // console.log(data.data, "data");
-      setTasks(data.data);
-    }).catch((error) => {
-      console.log(error, "error");
-    })
+    axios
+      .get("http://localhost:3000/api/todos")
+      .then((data) => {
+        // console.log(data.data, "data");
+        setTasks(data.data);
+      })
+      .catch((error) => {
+        console.log(error, "error");
+      });
   }, []);
 
   useEffect(() => {
@@ -35,13 +38,18 @@ function App() {
     //   name: tsk_name,
     // });
 
-    axios({
-      method: 'POST',
-      url: 'http://localhost:3000/api/todos',
-      params: { "name": "dasda" }
-    }).then((response) => {
-      console.log(response.data);
-    })
+    axios
+      .post("http://localhost:3000/api/todos", {
+        name: `${tsk_name.value}`,
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    // setTasks(tasks);
 
     // let jsonObj = {
     //   id: tasks.length,
@@ -51,45 +59,59 @@ function App() {
 
     // setTasks([...tasks, jsonObj]);
     tsk_name.value = "";
-}
+  };
 
-return (
-  <>
-    <div className="wrapper">
-      <div className="item960px bg-white shadow sm:rounded-lg">
-        <div className="px-4 py-5 sm:p-6">
-          <h3 className="text-base font-semibold leading-6 text-gray-900">TODOshechka</h3>
-          <div className="mt-2 max-w-xl text-sm text-gray-500">
-            <p>Please enter a task</p>
-          </div>
-          <div className="mt-5 sm:flex sm:items-center">
-            <div className="w-full sm:max-w-xs">
-              <label htmlFor="email" className="sr-only">
-                Email
-              </label>
-              <input
-                type="text"
-                name="taskName"
-                id="taskNameEl"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 input_placeholder_pad"
-                placeholder="profit quota 1530"
-              />
+  const deleteTask = (postIdToDelete) => {
+    axios
+      .delete(`http://localhost:3000/api/todos/${postIdToDelete}`)
+      .then((response) => {
+        console.log(`Deleted post with ID ${postIdToDelete}`);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    setTasks(tasks.filter(el => el.id !== postIdToDelete));
+  };
+  
+  return (
+    <>
+      <div className="wrapper">
+        <div className="item960px bg-white shadow sm:rounded-lg">
+          <div className="px-4 py-5 sm:p-6">
+            <h3 className="text-base font-semibold leading-6 text-gray-900">
+              TODOshechka
+            </h3>
+            <div className="mt-2 max-w-xl text-sm text-gray-500">
+              <p>Please enter a task</p>
             </div>
-            <button
-              type="button"
-              className="mt-3 inline-flex w-full items-center justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:ml-3 sm:mt-0 sm:w-auto"
-              onClick={() => getTask('taskNameEl')}
-            >
-              Save
-            </button>
+            <div className="mt-5 sm:flex sm:items-center">
+              <div className="w-full sm:max-w-xs">
+                <label htmlFor="email" className="sr-only">
+                  Email
+                </label>
+                <input
+                  type="text"
+                  name="taskName"
+                  id="taskNameEl"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 input_placeholder_pad"
+                  placeholder="profit quota 1530"
+                />
+              </div>
+              <button
+                type="button"
+                className="mt-3 inline-flex w-full items-center justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:ml-3 sm:mt-0 sm:w-auto"
+                onClick={() => getTask("taskNameEl")}
+              >
+                Save
+              </button>
+            </div>
           </div>
+          <Task tasks={tasks} />
+          {/* <p>{tasks}</p> */}
         </div>
-        <Task tasks={tasks} />
-        {/* <p>{tasks}</p> */}
       </div>
-    </div>
-  </>
-)
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/Task.jsx
+++ src/Task.jsx
@@ -1,43 +1,67 @@
-export default function Task({ tasks }) {
-    return (
-        <>
-            <div className="px-4 py-5 sm:p-6">
-                <h1 className="text-base font-semibold leading-6 text-gray-900">{(tasks.length > 0 ? "Tasks" : "Empty")}</h1>
-                {tasks.map((task, index) =>
-                    <div key={index} className="mt-4">
-                        <div className="rounded-md bg-gray-50 px-6 py-5 sm:flex sm:items-center sm:justify-between">
-                            <div className="sm:flex sm:items-center">
-                                <input
-                                    id="comments"
-                                    aria-describedby="comments-description"
-                                    name="comments"
-                                    type="checkbox"
-                                    className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                                    onChange={(index) => {
-                                        document.getElementById(index).style.textDecoration = "line-through";
-                                    }}
-                                />
-                                <div id={index} className="sm:ml-4 text-sm font-medium text-gray-900">{task.name}</div>
-                            </div>
-                            <div className="mt-4 sm:ml-6 sm:mt-0 sm:flex-shrink-0 gap-3">
-                                <button
-                                    type="button"
-                                    className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 mr-3"
-                                >
-                                    Edit
-                                </button>
-                                <button
-                                    type="button"
-                                    className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                                >
-                                    Delete
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                )}
-            </div>
-        </>
+import axios from "axios";
 
-    )
+export default function Task({ tasks }) {
+  const deleteTask = (postIdToDelete) => {
+    axios
+      .delete(`http://localhost:3000/api/todos/${postIdToDelete}`)
+      .then((response) => {
+        console.log(`Deleted post with ID ${postIdToDelete}`);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    // setTasks(tasks.filter(el => el.id !== postIdToDelete));
+  };
+
+  return (
+    <>
+      <div className="px-4 py-5 sm:p-6">
+        <h1 className="text-base font-semibold leading-6 text-gray-900">
+          {tasks.length > 0 ? "Tasks" : "Empty"}
+        </h1>
+        {tasks.map((task, index) => (
+          <div key={index} className="mt-4">
+            <div className="rounded-md bg-gray-50 px-6 py-5 sm:flex sm:items-center sm:justify-between">
+              <div className="sm:flex sm:items-center">
+                <input
+                  id="comments"
+                  aria-describedby="comments-description"
+                  name="comments"
+                  type="checkbox"
+                  className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                  // onChange={(index) => {
+                  //     document.getElementById(index).style.textDecoration = "line-through";
+                  // }}
+                />
+                <div
+                  id={index}
+                  className="sm:ml-4 text-sm font-medium text-gray-900"
+                >
+                  {task.name}
+                </div>
+              </div>
+              <div className="mt-4 sm:ml-6 sm:mt-0 sm:flex-shrink-0 gap-3">
+                <button
+                  type="button"
+                  className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 mr-3"
+                >
+                  Edit
+                </button>
+                <button
+                  type="button"
+                  className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                  // id="index"
+                  onClick={() => {
+                    deleteTask(`${index+1}`);
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
